cb203e72c54e014cd728a199d8fd719a744a095f
Marianne Pyon <mariannepyon@gmail.com>
2019-10-17 20:12:33 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+
+  belongs_to :network
+  has_many :characters, :actors
+
+  def actors_list
+    self.actors.collect do |actor|
+     "#{actor.name} - #{actor.show.name}"
+    end.join
+  end
 end
