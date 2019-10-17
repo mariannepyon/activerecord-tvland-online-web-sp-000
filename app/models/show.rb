4262b495d6f2cf15eff04f2086e4c378437329da
Marianne Pyon <mariannepyon@gmail.com>
2019-10-17 20:07:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,9 @@ class Show < ActiveRecord::Base
 
   belongs_to :network
   has_many :characters
+
+  def actors_list
+    self.characters.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    end.join
 end
